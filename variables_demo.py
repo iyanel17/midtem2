4889c8f5d8dde478ece3a2355b1211d77933c5a7
iyanel17 <iyanel689@gmail.com>
2026-04-10 12:36:26 +0800

    Add files via upload

---
+++ variables_demo.py
@@ -0,0 +1,9 @@
+student_name = "IYA NEL MANGARING"
+age = 20
+course = "BSIS"
+gpa = 1.75
+print("Student Name:", student_name)
+print("Age:", age)
+print("Course:", course)
+print("GPA:", gpa)
+#ISM
